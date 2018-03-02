5306fd479edb16afb402e157b62de7329f8927ae
slkk <644187060@qq.com>
2018-03-02 17:34:49 +0800

    QpushButton tooltip

--- com/slkk/pyqt/icon.py
+++ com/slkk/pyqt/icon.py
@@ -4,8 +4,10 @@
 this example shows an icon
 """
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import (QApplication, QWidget, QToolTip,
+                             QPushButton)
+from PyQt5.QtGui import QIcon, QFont
+
 
 class Example(QWidget):
     def __init__(self):
@@ -13,6 +15,14 @@ class Example(QWidget):
         self.initUI()
 
     def initUI(self):
+        QToolTip.setFont(QFont('SansSerif', 10))
+        self.setToolTip('This is a <b>QWidget</b> widget')
+
+        btn = QPushButton('Button', self)
+        btn.setToolTip('This is a <b>QpushButton</b> widget')
+        btn.resize(btn.sizeHint())
+        btn.move(50, 50)
+
         self.setGeometry(200, 300, 1000, 400)
         self.setWindowTitle('icon')
         self.setWindowIcon(QIcon('gvim.png'))
